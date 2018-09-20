f480975d1655407c2d8940d87ba670af0d0b68b1
Seema Joshi <seema@Seemas-MBP.fios-router.home>
2018-09-19 22:13:28 -0400

    Initial setup and HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+
+// DOM elements
+var startButtonHTML = document.getElementById("start-reset");
+var quwstionNumHTML = document.getElementById("questionNum");
+var questionHTML = document.getElementById("question");
+var winsHTML = document.getElementById("wins-count");
+var lossesHTML = document.getElementById("losses-count");
+var guessesLeftHTML = document.getElementById("guesses-left");
+
+

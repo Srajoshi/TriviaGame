64f11f9b6d2e768f5042189ae956b81ee36bc164
Seema Joshi <seema@Seemas-MacBook-Pro.local>
2018-09-21 10:09:35 -0400

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,9 @@
 var startButtonHTML = document.getElementById("startReset");
 var quwstionNumHTML = document.getElementById("questionNum");
 var questionHTML = document.getElementById("question");
-var correctHTML = document.getElementById("correct-count");
-var incorrectHTML = document.getElementById("incorrect-count");
-var answerHTML = document.getElementById("answer-msg");
+var correctHTML = document.getElementById("correctCount");
+var incorrectHTML = document.getElementById("incorrectCount");
+var answerHTML = document.getElementById("answerMsg");
 var buttonAHTML = document.getElementById("buttonA");
 var buttonBHTML = document.getElementById("buttonB");
 var buttonCHTML = document.getElementById("buttonC");
@@ -15,10 +15,7 @@ var correct = 0;
 var incorrect = 0;
 var answerMsg = "";
 var question = "";
-var choiceA = "";
-var choiceB = "";
-var choiceC = "";
-var choiceD = "";
+var choice = "";
 var gameFlag = false;
 var intervalId;
 var questions = [{
@@ -94,7 +91,7 @@ var questions = [{
   answer: "C"
 
 }];
-var i = 0
+// var i =0
 function display(i) {
 
   questionHTML.textContent = questions[i].question;
@@ -103,106 +100,78 @@ function display(i) {
   buttonCHTML.textContent = questions[i].choiceC;
   buttonDHTML.textContent = questions[i].choiceD;
 
+  answerHTML.textContent = "";
+
 };
 
 // display();
 
 
-
-// var clockRunning = false;
-
-// // Our stopwatch object
-// var stopwatch = {
-
-//   time: 20,
-
-//   reset: function() {
-
-//     stopwatch.time = 0;
-
-//     // DONE: Change the "display" div to "00:00."
-//     $("#timeLeft").text("00:00");
-
-//   },
-//   start: function() {
-
-//     // DONE: Use setInterval to start the count here and set the clock to running.
-//     if (!clockRunning) {
-//       intervalId = setInterval(stopwatch.count, 1000);
-//       clockRunning = true;
-//     }
-//   },
-//   stop: function() {
-
-//     // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-//     clearInterval(intervalId);
-//     clockRunning = false;
-//   },
-//   // recordLap: function() {
-
-//   //   // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-//   //   //       and save the result in a variable.
-//   //   var converted = stopwatch.timeConverter(stopwatch.time);
-
-//   //   // DONE: Add the current lap and time to the "laps" div.
-//   //   $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "</p>");
-
-//   //   // DONE: Increment lap by 1. Remember, we can't use "this" here.
-//   //   stopwatch.lap++;
-//   // },
-//   count: function() {
-
-//     // DONE: increment time by 1, remember we cant use "this" here.
-//     stopwatch.time--;
-
-//     // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-//     //       and save the result in a variable.
-//     var converted = stopwatch.timeConverter(stopwatch.time);
-//     console.log(converted);
-
-//     // DONE: Use the variable we just created to show the converted time in the "display" div.
-//     $("#timeLeft").text(converted);
-//   },
-//   timeConverter: function(t) {
-
-//     var minutes = Math.floor(t / 60);
-//     var seconds = t - (minutes * 60);
-
-//     if (seconds < 10) {
-//       seconds = "0" + seconds;
-//     }
-
-//     if (minutes === 0) {
-//       minutes = "00";
-//     }
-//     else if (minutes < 10) {
-//       minutes = "0" + minutes;
-//     }
-
-//     return minutes + ":" + seconds;
-//   }
-// };
-// $("#startReset").on("click", stopwatch.start);
-// gameFlag = true;
-display(i);
 // for (i=0 ; i < questions.length; i++) {
-// display(0);
+// display(i);
+// console.log(i);
+// $(".btnpick").on("click", function () {
 
+//  choice = $(this).val();
+// console.log(choice);
+// if (choice === questions[i].answer){
+// console.log(true)
+// // stop timer
+// // start the 5 sec timer
 
-
+// answerHTML.textContent = "Correct!";
+// } else {
+//   console.log(false)
+//   answerHTML.textContent = "Incorrect! the correct answer is: " + questions[i].answer + "."
 // }
 
-$(".btnpick").on("click", function () {
-  
- var choice = $(this).val();
-console.log(choice);
-if (choice === questions[i].answer){
-console.log(true)
-} else {
-  console.log(false)
+
+// });
+
+//  };
+
+var number = 10;
+var intervalId;
+
+
+//$("#stopReset").on("click", stop);
+// when gameFlag = true
+
+
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+  //gameFlag = true
 }
-  
 
-});
+//  The decrement function.
+function decrement() {
 
- 
+  //  Decrease number by one.
+  number--;
+
+  //  Show the number 
+  $("#timeLeft").text(number);
+
+  //  Once number hits zero...
+  if (number === 0) {
+
+    //run the stop function.
+    stop();
+
+    //  message time is up.
+    answerHTML.textContent = ("Time Up! The correct answer is: ");
+
+  }
+}
+
+//  The stop function
+function stop() {
+
+  clearInterval(intervalId);
+}
+
+//  Execute the run function.
+
+$("#startReset").on("click", run);
+$(".btnpick").on("click", stop);
